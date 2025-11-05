8d87491c66e7585cfde663092995f75941e1f546
Garnik <g.nersisyann@gmail.com>
2025-11-05 22:55:45 +0400

    chore: add nohup run script

--- simple_tracker/main.py
+++ simple_tracker/main.py
@@ -1,7 +1,7 @@
-from tracker import Tracker
+from .tracker import Tracker
 import time
 
-f = open("../config/config.txt","r")
+f = open("config/config.txt","r")
 interval = int(f.readline().split("=")[1].strip())
 print(interval)
 f.close()
