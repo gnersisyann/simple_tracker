6cbc57b785e028002544d4c03daad5e6554051b6
Garnik <g.nersisyann@gmail.com>
2025-11-05 22:44:45 +0400

    feat: add infinite loop that tracks counts

--- simple_tracker/main.py
+++ simple_tracker/main.py
@@ -1,4 +1,14 @@
+from tracker import Tracker
+import time
+
 f = open("../config/config.txt","r")
-interval = f.readline().split("=")[1].strip()
+interval = int(f.readline().split("=")[1].strip())
 print(interval)
 f.close()
+
+t = Tracker()
+while 1:
+	t.increment()
+	print(str(t))
+	t.save_to_file()
+	time.sleep(interval)
--- simple_tracker/tracker.py
+++ simple_tracker/tracker.py
@@ -3,7 +3,7 @@ class Tracker:
 	def increment(self):
 		self.count += 1
 	def save_to_file(self):
-		f = open("data.txt","w")
+		f = open("data.txt","a")
 		f.write(str(self.count))
 	def __str__(self):
 		return (f"Current count: {self.count}")
